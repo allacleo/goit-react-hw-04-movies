44c596b5cb4c493237c02e97fee66e62cd9c7580
Alla Sariieva <alla.cleo@gmal.com>
2019-12-20 11:38:17 +0200

    Updates

---
+++ precache-manifest.930b61056d757e45d5d04dbb378fc71b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b0ff7c8a6f3e5d452f68889b73d645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069fa901057f091a4fbf",
+    "url": "/static/css/main.22ddfb63.chunk.css"
+  },
+  {
+    "revision": "ca945652ee4aa88b6c81",
+    "url": "/static/js/2.ca4ade5b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ca4ade5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "069fa901057f091a4fbf",
+    "url": "/static/js/main.cdd10149.chunk.js"
+  },
+  {
+    "revision": "bcf6a1d3bb3d348de568",
+    "url": "/static/js/runtime-main.80cdc8d9.js"
+  }
+]);
